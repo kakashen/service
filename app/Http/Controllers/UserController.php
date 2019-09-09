b05402e6f914870616b07ac050ac370d50bb7e9f
jizuoliang <1519176958@qq.com>
2019-09-09 15:49:14 +0800

    登录退出修改

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,19 +21,26 @@ class UserController extends Controller
     //登录
     public function login(Request $request)
     {
-        if ($request->has('username') && $request->has('password')) {
-            $user = User::where('username', '=', $request->input('username'))->where('password', '=', sha1($this->salt . $request->input('password')))->first();
-            if ($user) {
-                $user->api_token = uniqid();
-                $user->save();
-                return response()->json(['message' => '登录成功', 'code' => 200,
-                    'api_token' => $user->api_token]);
-            } else {
-                return response()->json(['message' => '账号或密码错误', 'code' => 0]);
-            }
-        } else {
-            return response()->json(['message' => '未知错误', 'code' => 0]);
+        $username = $request->get('username');
+        if (empty($username)) {
+            return response()->json(['message' => '请输入账号', 'code' => 0]);
         }
+        $password = $request->get('password');
+        if (empty($username)) {
+            return response()->json(['message' => '请输入密码', 'code' => 0]);
+        }
+
+        $user = User::where('username', $username)->where('password', sha1($this->salt . $password))->first();
+        if ($user) {
+            $user->api_token = uniqid();
+            $user->status = $request->get('status') ?? 1; // 登录状态
+            $user->save();
+            return response()->json(['message' => '登录成功', 'code' => 200,
+                'data' => $user]);
+        } else {
+            return response()->json(['message' => '账号或密码错误', 'code' => 0]);
+        }
+
     }
 
     //注册
@@ -58,7 +65,12 @@ class UserController extends Controller
     public function logout()
     {
         // 过期的 token 不应该还请求后端 delete（也就不存在服务端报错），直接前端清除缓存退出即可
-        return response()->json(['message' => '退出成功', 'code' => 200]);
+        $ret = User::where('id', Auth::user()->id)->update('status', 0);
+        if ($ret) {
+            return response()->json(['message' => '退出成功', 'code' => 200]);
+        }
+        return response()->json(['message' => '退出失败', 'code' => 0]);
+
     }
 
 
