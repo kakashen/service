8c80ae574f698a4875e25f2a64e0e9ea11fa28bd
jizuoliang <1519176958@qq.com>
2019-09-10 17:09:57 +0800

    修改model : user->staff

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
+use App\Staff;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -12,7 +12,7 @@ class UserController extends Controller
     private $user;
     private $salt;
 
-    public function __construct(User $user)
+    public function __construct(Staff $user)
     {
         $this->user = $user;
         $this->salt = "userLoginRegister";
@@ -30,7 +30,7 @@ class UserController extends Controller
             return response()->json(['message' => '请输入密码', 'code' => 0]);
         }
 
-        $user = User::where('username', $username)->where('password', sha1($this->salt . $password))->first();
+        $user = Staff::where('username', $username)->where('password', sha1($this->salt . $password))->first();
         if ($user) {
             $user->api_token = uniqid();
             $user->status = $request->get('status') ?? 1; // 登录状态
@@ -47,7 +47,7 @@ class UserController extends Controller
     public function register(Request $request)
     {
         if ($request->has('username') && $request->has('password')) {
-            $user = new User;
+            $user = new Staff;
             $user->username = $request->input('username');
             $user->password = sha1($this->salt . $request->input('password'));
             $user->api_token = Str::random(32);;
@@ -65,7 +65,7 @@ class UserController extends Controller
     public function logout()
     {
         // 过期的 token 不应该还请求后端 delete（也就不存在服务端报错），直接前端清除缓存退出即可
-        $ret = User::where('id', Auth::user()->id)->update('status', 0);
+        $ret = Staff::where('id', Auth::user()->id)->update('status', 0);
         if ($ret) {
             return response()->json(['message' => '退出成功', 'code' => 200]);
         }
