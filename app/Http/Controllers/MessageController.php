f22111892d9a5dcbd16c874caa76f583ddf56a2d
jizuoliang <1519176958@qq.com>
2019-09-10 15:08:44 +0800

    发送消息

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Communication;
 use App\Model\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -21,9 +22,9 @@ class MessageController extends Controller
         $from = $user->id; // 发送人id
         $to = $request->get('to'); // 接收人id
         $content = $request->get('content');
-        $direction = $request->get('direction');
+        $direction = $request->get('direction'); // 1客户发给客服, 2客服发给客户
         $type = $request->get('type', 1);
-        $communication_id = $request->get('communication_id', 1);
+        $communication_id = $request->get('communication_id');
 
         if (!isset($to)) {
             return response()->json(['message' => '接收人不能为空', 'code' => 0]);
@@ -33,6 +34,21 @@ class MessageController extends Controller
         }
         if (!isset($direction)) {
             return response()->json(['message' => '方向不能为空', 'code' => 0]);
+        }
+
+        if (!isset($communication_id)) {
+
+            $comm = new Communication();
+            $communication_id = $comm->insertGetId([
+                'client_id' => $from,
+                'user_id' => $to,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+
+            ]);
+            if (!$communication_id) {
+                return response()->json(['message' => '创建会话失败', 'code' => 0]);
+            }
         }
 
         $message = $this->message;
